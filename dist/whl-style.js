b787f4134ed0c40a62b0991b6a0c2b41c0a83e9f
Mechanic <85401098+K93MSY@users.noreply.github.com>
2023-02-03 01:13:24 +0000

    Added card component

--- dist/whl-style.js
+++ dist/whl-style.js
@@ -5,5 +5,6 @@ var __webpack_exports__ = {};
 
 
 
+
 /******/ })()
 ;
--- src/build.js
+++ src/build.js
@@ -2,3 +2,4 @@ import './component/button.css';
 import './component/text.css';
 import './component/shape.css';
 import './component/grid.css';
+import './component/card.css';
